cc5b4ed217ef765e75f205036c979c0a28f3b60d
Your Name <you@example.com>
2017-02-23 13:06:41 -0500

    updated

--- cross_validation/cross_validation.py
+++ cross_validation/cross_validation.py
@@ -1,10 +1,10 @@
 from sklearn.datasets import load_digits
+from sklearn.preprocessing import label_binarize
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import cross_val_score
 import matplotlib.pyplot as plt 
 
 def main():
-    print("hw2")
 
     digits = load_digits()
     print("digits.data.shape: " + str(digits.data.shape))
@@ -18,10 +18,24 @@ def main():
     #plt.matshow(digits.images[1796]) 
     #plt.show()
 
+    # convert class labels to be 1 if digit is 9; 0 for all other digits
+    #binarized_digits = label_binarize([9], digits.target)
+    binarized_digits = []
+    for i in range(len(digits.target)):
+        if digits.target[i] == 9:
+            binarized_digits.append(9)
+        else:
+            binarized_digits.append(0)
+    #print(len(binarized_digits))
+    #print(binarized_digits)
+
     # 1-nearest neighbor
     print("")
     print("running 1-nearest neighbor...")
     clf = KNeighborsClassifier(n_neighbors=1)
+    scores = cross_val_score(clf, digits.data, binarized_digits, cv=10)
+
+    print("cross_val_scores for 1-nearest neighbor: " + str(scores))
     
 
 if __name__ == '__main__':
