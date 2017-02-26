739d7de8a2ac2948f320c3bf4a233142edbe4b92
user <certemail@hotmail.com>
2017-02-26 10:17:25 -0500

    updated

--- cross_validation/cross_validation.py
+++ cross_validation/cross_validation.py
@@ -1,7 +1,7 @@
 from sklearn.datasets import load_digits
-from sklearn.preprocessing import label_binarize
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
 import matplotlib.pyplot as plt 
 
@@ -9,42 +9,48 @@ def display_accuracy(scores):
     print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
 '''convert class labels to be 1 if digit is 9; 0 for all other digits'''
-def convert_class_labels(d):
+def convert_class_labels(d_target):
     converted_digits = []
-    for i in range(len(d.target)):
-        if d.target[i] == 9:
-            converted_digits.append(9)
+    for i in range(len(d_target)):
+        if d_target[i] == 9:
+            converted_digits.append(1)
         else:
             converted_digits.append(0)
     return converted_digits
 #---convert_class_labels---------------------------------
 
 
-def run_nearest_neighbor(digits):
+def run_nearest_neighbor(X_digits, y_digits):
     print("running 1-nearest neighbor...")
     clf = KNeighborsClassifier(n_neighbors=1)
-    scores = cross_val_score(clf, digits.data, convert_class_labels(digits), cv=10)
+    scores = cross_val_score(clf, X_digits, y_digits, cv=10)
     print("cross_val_scores for 1-nearest neighbor:")
-    for i in range(len(scores)):
-        print("\t" + str(scores[i]))
+    [print("\t" + str(s)) for s in scores]
     display_accuracy(scores)
 #---run_nearest_neighbor---------------------------------
 
 	
-def run_decision_tree(digits):
+def run_decision_tree(X_digits, y_digits):
     print("running decision tree...")
     clf = DecisionTreeClassifier()
-    scores = cross_val_score(clf, digits.data, convert_class_labels(digits), cv=10)
+    scores = cross_val_score(clf, X_digits, y_digits, cv=10)
     print("cross_val_scores for decision tree: ")
-    for i in range(len(scores)):
-        print("\t" + str(scores[i]))
+    [print("\t" + str(s)) for s in scores]
     display_accuracy(scores)
 #---run_decision_tree------------------------------------
 
 
-def run_logistic_regression(digits):
+def run_logistic_regression(X_digits, y_digits):
     print("running logistic regression...")
+    clf = LogisticRegression()
+    scores = cross_val_score(clf, X_digits, y_digits, cv=10)
     print("cross_val_scores for logistic regression: ")
+    [print("\t" + str(s)) for s in scores]
+    display_accuracy(scores)
+
+    clf.fit(X_digits, y_digits)
+    print("coefficients: " + str(clf.coef_))
+
 #---run_logistic_regression------------------------------------
 
 
@@ -55,22 +61,22 @@ def main():
     print(digits.data)
     print("digits.target: ")
     print(digits.target)
-    l = list(digits.target)
-    print("number of 9s in digits.target: " +  str(l.count(9)))
-    cd = convert_class_labels(digits).count(9)
-    print("number of 9s in digits.target converted: " +  str(cd))
+    print(convert_class_labels(digits.target).count(1))
+    print(convert_class_labels(digits.target).count(0))
+
+    X_digits, y_digits = digits.data, convert_class_labels(digits.target)
 
     # 1-nearest neighbor
     print("==========================")
-    run_nearest_neighbor(digits)
+    run_nearest_neighbor(X_digits, y_digits)
     print("--------------------------")
     
     # decision tree
-    run_decision_tree(digits)
+    run_decision_tree(X_digits, y_digits)
     print("--------------------------")
 
     # logistic regression
-    run_logistic_regression(digits)
+    run_logistic_regression(X_digits, y_digits)
     print("--------------------------")
 
 if __name__ == '__main__':
