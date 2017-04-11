81ebe078440a48097a991e047a0500cc0efcecee
user <certemail@hotmail.com>
2017-04-11 16:59:04 -0400

    updated

--- project/combine_feature_vectors_and_build_model.py
+++ project/combine_feature_vectors_and_build_model.py
@@ -8,8 +8,7 @@ import itertools as it
 features_and_vector_lengths = {}
 
 def display_data_matrix(data):
-    print("============")
-    print("complete data matrix:")
+    print("\n========DATA MATRIX======")
     print(*data, sep='\n')
 
     print("~~~~~~~~~~~~~")
@@ -22,7 +21,10 @@ def display_data_matrix(data):
     print("Y: ")
     for y in Y:
         print(y)
-
+    print("========DATA SUMMARY======")
+    print("matrix dimensions: " + str(len(data)) + " x " + str(len(data[0])))
+    print("number of rows: " + str(len(data)))
+    print("length of feature vector: " + str(len(X[0])))
 
 def convert_label(label_as_list):
     # index of where "1" indicates the class (0,1,2,3)
@@ -31,20 +33,18 @@ def convert_label(label_as_list):
 
 def get_all_features_and_vector_lengths(path_to_all_possible_feature_values_filename):
     # open json file containing all possible features and feature_values
-    print("All possible features and count of feature-values in dataset:")
+    print("ALL POSSIBLE FEATURES AND COUNT OF FEATURE-VALUES IN DATASET:")
     with open(path_to_all_possible_feature_values_filename) as data_file:
         data_set_features_values = json.load(data_file)
-
         for feature in data_set_features_values:
             print(feature + ": " + str(len(data_set_features_values[feature])))
             features_and_vector_lengths[feature] = len(data_set_features_values[feature])
-    print(features_and_vector_lengths)
        
-
 def build_matrix_from_selected_features(path_to_dataset, path_to_list_of_features_to_train):
     # read in desired features for use in the model from text file and sort alphabetically
     features_to_use = sorted([line.rstrip() for line in open(path_to_list_of_features_to_train)])
 
+    print("-----------")
     print("FEATURES USED FOR THIS MODEL:", *features_to_use, sep='\n')
     print("-----------")
 
@@ -58,43 +58,39 @@ def build_matrix_from_selected_features(path_to_dataset, path_to_list_of_feature
 
             row = []
             for feature in features_to_use:
-                print(feature)
 
-                # TODO include a sanity check to gracefully handle the case where the json file does not have a feature
-                # does not have a feature - abort? or just add in a vector of all zeros for that missing feature?
+                # if feature is not present in sample, pad feature vector with zeros
                 if feature not in malware_sample:
                     vector_len = features_and_vector_lengths[feature]
-                    print("WARNING: " + feature + " NOT present in " + filename + " (padding feature vector with " + str(vector_len) + " 0s)")
-                    temp = list(map(lambda x: 9, range(vector_len)))     #TODO - REPLACE BACK WITH ZERO (DEBUG ONLY)
+                    print("  " + feature + ": " + "NOT present in " + filename + " (padding feature vector with " + str(vector_len) + " zeros)")
+                    temp = list(map(lambda x: 0, range(vector_len)))     
                     row.extend(temp)
-                    print("row: " + str(row))
-
+                    #print("  row: " + str(row))
                 else:
-                    print(malware_sample[feature])
-
+                    print("  " + feature + ":" + str(malware_sample[feature]))
                     # collapse all features into one list (X)
                     row.extend(malware_sample[feature])
-                    print("row: " + str(row))
+                    #print("row: " + str(row))
 
             class_label = convert_label(malware_sample['label'])
             row.extend(class_label)
-            print("row: " + str(row))
+            print("  label: " + str(class_label))
+            print("  row: " + str(row))
 
             # append X to matrix
             data_matrix.append(row)
 
     return data_matrix
 
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("path_to_dataset", help="path to dataset")
+    parser.add_argument("path_to_normalized_dataset", help="path to normalized dataset")
     parser.add_argument("path_to_all_possible_feature_values_filename", help="path to all possible feature values .json file ")
-    parser.add_argument("path_to_list_of_features_to_train", help="path to .txt file with list of features to concactenate into one vector")
+    parser.add_argument("path_to_list_of_features_to_train", help="path to .txt file with list of features to use for training")
     args = parser.parse_args()
 
     get_all_features_and_vector_lengths(args.path_to_all_possible_feature_values_filename)
-    data = build_matrix_from_selected_features(args.path_to_dataset, args.path_to_list_of_features_to_train)
+    data = build_matrix_from_selected_features(args.path_to_normalized_dataset, args.path_to_list_of_features_to_train)
 
     # print out data (for debugging)
     display_data_matrix(data)
