b51720069c7bd0d2d95aba4c1d3529e8dd068ede
rob <certemail@hotmail.com>
2017-04-17 09:15:29 -0400

    updated

--- qlearning/qlearn.py
+++ qlearning/qlearn.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 import random
+import argparse
 
-#----GLOBAL VARIABLES--------------------
-NUM_POSSIBLE_ACTIONS = 4
-POSSIBLE_ACTIONS = ['UP', 'DOWN', 'LEFT', 'RIGHT']
-
+#---global variables-----------------------------------------------------------
 # grid dimensions
 NUM_ROWS = 5 
-NUM_COLS = 5
+NUM_COLS = 5 
+
+# learning parameters
+GAMMA = 0.9
+LEARNING_RATE = 0.5
+
+# exploration probability
+EPSILON = 0.01
+
+# possible actions
+NUM_POSSIBLE_ACTIONS = 4
+POSSIBLE_ACTIONS = ['UP', 'DOWN', 'LEFT', 'RIGHT']
 
 # rewards
 REWARD_ON_EXIT = 1.0
@@ -22,44 +31,56 @@ END_STATE_COL = NUM_COLS - 1
 PENALTY_ROW = END_STATE_ROW - 1 
 PENALTY_COL = END_STATE_COL    
 
-# number of episodes to train
-NUM_EPISODES = 100
-
-GAMMA = 0.9
-LEARNING_RATE = 0.5
-EPSILON = 0.01
-
 # initialize goal state (lower-right-hand corner)
 GOAL_STATE = (NUM_ROWS * NUM_COLS) - 1
 GOAL_STATE_VALUE = 0.0
-#----GLOBAL VARIABLES--------------------
+#---global variables-----------------------------------------------------------
+
+def initialize_grid():
+    print("initializing grid...")
+    grid_world = [[0.0 for col in range(NUM_COLS)] for row in range(NUM_ROWS)]
+    print("exit position:    " + "(" + str(END_STATE_ROW) + "," + str(END_STATE_COL) + ")")
+    print("penalty position: " + "(" + str(PENALTY_ROW) + "," + str(PENALTY_COL) + ")")
+    grid_world[PENALTY_ROW][PENALTY_COL] = REWARD_IN_PENALTY_SQUARE
+    grid_world[END_STATE_ROW][END_STATE_COL] = REWARD_ON_EXIT
+    return grid_world
+
+def initialize_q_table():
+    NUM_ROWS_IN_Q_TABLE = (NUM_ROWS * NUM_ROWS) - 1
+    print("initializing q table...")
+    print("number of rows in q-table: " + str(NUM_ROWS_IN_Q_TABLE))
+    q_table = [[0.0 for col in range(NUM_POSSIBLE_ACTIONS)] for row in range(NUM_ROWS_IN_Q_TABLE)]
+    return q_table
 
 def display_grid(grid_world):
     for row in grid_world:
         print(row)
     print("------------")
+#---display_grid---------------------------------------------------------------
 
 def display_q_table(q_table):
     # show q_table, rounded to 3 decimal places
     state_number = 0
     print('\t    {}      {}      {}      {}'.format('UP', 'DOWN', 'LEFT', 'RIGHT'))
     for row in q_table:
-        rounded_row = [ '%.3f' % round(elem, 3) for elem in row]
-        print("state " + str(state_number) + ":" + str(rounded_row) + "--> max Q: " + str(max(row)))
+        rounded_row = [ '%.3f' % elem for elem in row]
+        print('state {}: {} --> max-q: {:.3f}'.format(state_number, rounded_row, max(row)))
         state_number += 1
     print("------------")
+#---display_q_table--------------------------------------------------------------
 
 def convert_current_state_to_coordinates(current_state):
     row = current_state // NUM_ROWS
     col = current_state %  NUM_COLS
     return (row, col)
+#---convert_current_state_to_coordinates----------------------------------------
 
 def convert_coordinates_to_current_state(row, col):
     return (row * NUM_ROWS + col)
+#---convert_coordinates_to_current_state---------------------------------------
 
 def choose_action(current_state):
     print("choose_action()...")
-
     # exploration (do completely random action)
     rand = random.random()
     if rand < EPSILON:
@@ -88,6 +109,7 @@ def choose_action(current_state):
         idx = q_table[current_state].index(max_q_value)
         action = POSSIBLE_ACTIONS[idx]        
         return action
+#---choose_action--------------------------------------------------------------
 
 def get_next_state(current_state, action):
 #returns the next state (grid cell) after moving
@@ -129,6 +151,8 @@ def get_next_state(current_state, action):
             return(current_state)
         else:
             return(convert_coordinates_to_current_state(x, y+1))
+#---get_next_state-------------------------------------------------------------
+
 
 def compute_immediate_reward_and_update_q_table(previous_state, action_taken, new_state):
     global GOAL_STATE_VALUE
@@ -145,7 +169,7 @@ def compute_immediate_reward_and_update_q_table(previous_state, action_taken, ne
 
         # compute new q-value (using GOAL_STATE_VALUE as 'q_value' for terminal state)
         new_q_value_for_previous_state = previous_state_q_value_for_action_just_taken + \
-                                            (LEARNING_RATE * (REWARD_FOR_MAKING_ANY_MOVE + (GAMMA * GOAL_STATE_VALUE) - previous_state_q_value_for_action_just_taken))
+            (LEARNING_RATE * (REWARD_FOR_MAKING_ANY_MOVE + (GAMMA * GOAL_STATE_VALUE) - previous_state_q_value_for_action_just_taken))
         
         print('updated q-value {}'.format(new_q_value_for_previous_state))
 
@@ -168,31 +192,29 @@ def compute_immediate_reward_and_update_q_table(previous_state, action_taken, ne
 
         # compute new q-value
         new_q_value_for_previous_state = previous_state_q_value_for_action_just_taken + \
-                                            (LEARNING_RATE * (REWARD_FOR_MAKING_ANY_MOVE + (GAMMA * max_q_of_new_state) - previous_state_q_value_for_action_just_taken))
+            (LEARNING_RATE * (REWARD_FOR_MAKING_ANY_MOVE + (GAMMA * max_q_of_new_state) - previous_state_q_value_for_action_just_taken))
 
         print('updated q-value {}'.format(new_q_value_for_previous_state))
 
         # update q-table for previous state for the action just taken
         q_table[previous_state][act_action_idx] = new_q_value_for_previous_state
+#---compute_immediate_reward_and_update_q_table--------------------------------
+
 
 if __name__ == '__main__':
-    # initialize grid
-    print("initializing grid...")
-    grid_world = [[0.0 for col in range(NUM_COLS)] for row in range(NUM_ROWS)]
-    print("exit position:    " + "(" + str(END_STATE_ROW) + "," + str(END_STATE_COL) + ")")
-    print("penalty position: " + "(" + str(PENALTY_ROW) + "," + str(PENALTY_COL) + ")")
-    grid_world[PENALTY_ROW][PENALTY_COL] = REWARD_IN_PENALTY_SQUARE
-    grid_world[END_STATE_ROW][END_STATE_COL] = REWARD_ON_EXIT
-    display_grid(grid_world) 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("num_episodes", help="number of episodes")
+    args = parser.parse_args()
 
-    # initialize q-table
-    NUM_ROWS_IN_Q_TABLE = (NUM_ROWS * NUM_ROWS) - 1
-    print("initializing q table...")
-    print("number of rows in q-table: " + str(NUM_ROWS_IN_Q_TABLE))
-    q_table = [[0.0 for col in range(NUM_POSSIBLE_ACTIONS)] for row in range(NUM_ROWS_IN_Q_TABLE)]
+    num_episodes = int(args.num_episodes)
+
+    grid_world = initialize_grid()
+    q_table = initialize_q_table()
+    display_grid(grid_world) 
     display_q_table(q_table)
 
-    for i in range(NUM_EPISODES):
+    print('running {} episodes'.format(num_episodes))
+    for i in range(num_episodes):
         # start from beginning (upper left-hand corner at (0,0), reset after each episode and goal is reached)
         current_state = 0
         num_steps_until_goal_reached = 0
