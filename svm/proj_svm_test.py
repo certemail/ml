0df8240627996a8a963b1c57f07996b41fa4cfa0
Your Name <you@example.com>
2017-05-04 10:16:05 -0400

    updated

--- svm/proj_svm_test.py
+++ svm/proj_svm_test.py
@@ -4,53 +4,67 @@ from matplotlib import style
 style.use("ggplot")
 from sklearn import svm
 
-X =    [[ 4,  4,  3 ],  
-        [ 4,  2,  3 ],  
-        [ 1,  2,  3 ],  
-        [ 4,  2,  3 ],  
-        [ 4, -4,  3 ],  
-        [ 0,  0,  3 ],  
-        [ 0,  0,  3 ],  
-        [ 4, -2,  3 ]]  
+#X =    [[ 4,  4,  3,  4 ],  
+#        [ 4,  2, -2,  2 ],  
+#        [ 1,  2,  3,  2 ],  
+#        [ 4,  2,  3,  2 ],  
+#        [ 4, -4,  2, -4 ],  
+#        [ 0,  0,  3,  0 ],  
+#        [ 0,  0,  1,  0 ],  
+#        [ 4, -2, -3, -2 ]]  
+#
+#y = [1,2,3,1,0,2,0,3]
 
-# three classes
-y = [1,2,1,1,0,2,0,2]
+X = [[1,0,1,1,1,0,0,0,1,0,0,0,1,0,0,0,1,0,1,1,1,1,0,1,0,0,0,0,0,0,0,1,0,0],
+     [1,0,1,0,1,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,1,0,1,0,0,0,0,1,0], 
+     [0,0,1,1,1,0,0,0,1,0,0,1,1,0,1,0,1,0,1,1,1,1,0,1,0,0,1,0,0,0,0,1,0,0], 
+     [1,0,1,1,1,0,0,0,1,0,0,1,1,1,0,0,1,0,0,1,1,1,0,1,1,0,0,0,1,0,1,1,0,0],
+     [0,0,1,1,1,0,0,0,1,0,0,1,0,1,0,0,1,0,0,1,1,1,0,1,1,0,0,0,1,0,1,1,0,1],
+     [1,1,1,1,0,0,0,0,1,0,0,1,1,1,0,0,1,0,0,1,0,1,0,0,0,0,0,0,1,0,1,1,1,0]]
 
+y = [0,3,2,1,0,1]
 # convert X to numpy ndarray
 X = np.array(X)
 
 # print original dataset feature values
-print("dataset:")
+print("dataset (X):")
 print(X)
+print('{}: {}'.format("length of feature vector", len(X[0])))
 print()
+print("classification (y):")
+print(y)
+print('{}: {}'.format("number of classes", len(set(y))))
+
+print("*************\n")
 
 clf = svm.SVC(kernel='linear', C = 1.0)
 clf.fit(X,y)
 print('{} {}'.format("clf:", clf))
-
-w = clf.coef_[0]
-print('{}: {}'.format("clf.coef_[0] (w)", w))
-print('{}: {}'.format("clf.intercept_[0]", clf.intercept_[0]))
-print('{}: {}'.format("number of support vectors for each class:", clf.n_support_))
-print('{}: {}'.format("support vectors", clf.support_vectors_))
 print()
 
-#a = -w[0] / w[1]
-#print('{} {}'.format("a:", a))
-#
-#test_point_1 = [[ 0, -1 ]]
-#test_point_2 = [[ 1,  2 ]]
-#test_point_3 = [[ 2,  3 ]]
-#print('{} {} --> class {}'.format("predicting point:", test_point_1, clf.predict(test_point_1)))
-#print('{} {} --> class {}'.format("predicting point:", test_point_2, clf.predict(test_point_2)))
-#print('{} {} --> class {}'.format("predicting point:", test_point_3, clf.predict(test_point_3)))
+w = clf.coef_[0]
+print("clf.coef_[0] (w):")
+print("feature vector length: " + str(len(clf.coef_[0])))
+print(w)
+print()
 
-#xx = np.linspace(0,4)
-#yy = a * xx - clf.intercept_[0] / w[1]
-#
-#h0 = plt.plot(xx, yy, 'k-', label="non weighted div")
-#
-#plt.scatter(X[:, 0], X[:, 1], c = y)
-#plt.legend()
-#plt.show()
+print('{}: {}'.format("clf.intercept_[0]", clf.intercept_[0]))
 
+print('{}: {}'.format("number of classes", len(clf.n_support_)))
+print('{}: {}'.format("number of support vectors for each class:", clf.n_support_))
+
+print("support vectors:")
+print(clf.support_vectors_)
+print()
+
+# predict
+print("Make predictions...(testing on same rows in X)")
+print(clf.predict( [[1,0,1,1,1,0,0,0,1,0,0,0,1,0,0,0,1,0,1,1,1,1,0,1,0,0,0,0,0,0,0,1,0,0]] )) 
+print(clf.predict( [[1,0,1,0,1,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,1,0,1,0,0,0,0,1,0]] ))
+print(clf.predict( [[0,0,1,1,1,0,0,0,1,0,0,1,1,0,1,0,1,0,1,1,1,1,0,1,0,0,1,0,0,0,0,1,0,0]] ))
+print(clf.predict( [[1,0,1,1,1,0,0,0,1,0,0,1,1,1,0,0,1,0,0,1,1,1,0,1,1,0,0,0,1,0,1,1,0,0]] ))
+print(clf.predict( [[0,0,1,1,1,0,0,0,1,0,0,1,0,1,0,0,1,0,0,1,1,1,0,1,1,0,0,0,1,0,1,1,0,1]] ))
+print(clf.predict( [[1,1,1,1,0,0,0,0,1,0,0,1,1,1,0,0,1,0,0,1,0,1,0,0,0,0,0,0,1,0,1,1,1,0]] ))
+print
+print("correct classifications: ")
+print(y)
