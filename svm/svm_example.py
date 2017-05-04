6c6a095782811d2ded8b105e465a1602d2b7b7b7
Your Name <you@example.com>
2017-05-04 08:51:21 -0400

    updated

--- svm/svm_example.py
+++ svm/svm_example.py
@@ -4,14 +4,14 @@ from matplotlib import style
 style.use("ggplot")
 from sklearn import svm
 
-X =          [[4,4],  # class A (+)
-              [4,2],  # class A (+)
-              [1,2],  # class A (+)
-              [4,2],  # class A (+)
-              [4,-4], # class B (-)
-              [0,0],  # class B (-)
-              [0,0],  # class B (-)
-              [4,-2]] # class B (-)
+X =    [[ 4,  4 ],  # class A (+)
+        [ 4,  2 ],  # class A (+)
+        [ 1,  2 ],  # class A (+)
+        [ 4,  2 ],  # class A (+)
+        [ 4, -4 ],  # class B (-)
+        [ 0,  0 ],  # class B (-)
+        [ 0,  0 ],  # class B (-)
+        [ 4, -2 ]]  # class B (-)
 
 y = [1,1,1,1,0,0,0,0]
 
