ef31d8d1f892af1774a4e9f73e10afa2e3095321
Your Name <you@example.com>
2017-05-04 08:42:53 -0400

    updated

--- svm/svm_example.py
+++ svm/svm_example.py
@@ -4,39 +4,32 @@ from matplotlib import style
 style.use("ggplot")
 from sklearn import svm
 
+X = np.array([[4,4],  # class A (+)
+              [4,2],  # class A (+)
+              [1,2],  # class A (+)
+              [4,2],  # class A (+)
+              [4,-4], # class B (-)
+              [0,0],  # class B (-)
+              [0,0],  # class B (-)
+              [4,-2]])# class B (-)
 
-#def my_kernel(X, y):
-#    X_new = np.array([  [4,4],
-#                        [4,2],
-#                        [1,2],
-#                        [4,2],
-#                        [4,-4],
-#                        [0,0],
-#                        [0,0],
-#                        [4,-2]])
-#    y_new = y
-#    return(X_new, y)
-
-
-X = np.array([  [2,2],
-                [-2,-1],
-                [1,2],
-                [2,1],
-                [-2,2],
-                [0,2],
-                [0,-1],
-                [2,-1]])
-
-y = [0,0,0,0,1,1,1,1]
+y = [1,1,1,1,0,0,0,0]
 
 clf = svm.SVC(kernel='linear', C = 1.0)
 clf.fit(X,y)
 
 w = clf.coef_[0]
-print(w)
-print(clf.intercept_[0])
+print('{}: {}'.format("clf.coef_[0] (w)", w))
+print('{}: {}'.format("clf.intercept_[0]", clf.intercept_[0]))
+print('{}: {}'.format("number of support vectors for each class:", clf.n_support_))
+print('{}: {}'.format("support vectors", clf.support_vectors_))
+print()
 
 a = -w[0] / w[1]
+print('{} {}'.format("a:", a))
+
+test_point = [[0,-1]]
+print('{} {} --> class {}'.format("predicting point:", test_point, clf.predict(test_point)))
 
 xx = np.linspace(0,4)
 yy = a * xx - clf.intercept_[0] / w[1]
