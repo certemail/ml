02b39175fc01c908a87c3d612655712c3964189a
rob <certemail@hotmail.com>
2017-05-06 10:24:41 -0400

    updated

--- project/copy_subset_of_dataset_to_sample_folder.sh
+++ project/copy_subset_of_dataset_to_sample_folder.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 if [[ -z "$2" ]]; then
-  echo "usage: ${0} [subfolder_to_copy] [number_of_files_to_copy]"
+  echo "usage: ${0} [number_of_files_to_copy] [subfolder_to_copy] "
   echo ""
   echo "example: to copy 50 Zeus files from the full dataset into the sample_dataset directory:"
-  echo "${0} Zeus 50"
+  echo "${0} 50 Zeus"
   echo ""
   exit
 fi
 
+NUM_FILES_TO_COPY=${1}
+SUBFOLDER=${2}
 PATH_TO_DATASET=${HOME}/MalwareTrainingSets/trainingSets
-SUBFOLDER=$1
 PATH_TO_SAMPLE_DATASET=${HOME}/ml/project/sample_dataset
-NUM_FILES_TO_COPY=50
 
 # copy files into sample_dataset directory
 for file in $(ls -p ${PATH_TO_DATASET}/${SUBFOLDER} | grep -v / | tail -${NUM_FILES_TO_COPY}); do 
     echo ${PATH_TO_DATASET}/${SUBFOLDER}/${file};
     cp ${PATH_TO_DATASET}/${SUBFOLDER}/${file} ${PATH_TO_SAMPLE_DATASET}
 done
+
+echo "copied ${NUM_FILES_TO_COPY} ${SUBFOLDER} ..."
